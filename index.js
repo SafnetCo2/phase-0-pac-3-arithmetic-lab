6e5adef1b3b61ac91468320f03b4a906c8087720
nzi19001@byui.edu <nzi19001@byui.edu>
2023-12-13 15:28:03 +0300

    completed assigment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b){
+    return a+b;         //add a+b
+}
+add(1+2);
+
+function subtract(a,b){
+    return a-b;         //substract a-b
+}
+subtract(5-3);
+
+function multiply(a,b){         //multiply a*b 
+    return a*b
+}
+multiply(2*4);
+
+function divide(a,b){           //divide a/b
+    return a/b;
+
+}
+divide(10/5)
+
+function increment(n){  //increment
+    return n+1
+}
+increment(4)
+//decrement by 1
+function decrement(n){
+    return n-1
+}
+decrement(5)
+//parse an interger using parseInt
+function makeInt(n){
+    return  parseInt (n,10)
+}
+makeInt(2)
+
+//parseFloat
+function preserveDecimal(n){
+    return parseFloat(n ,40.9)
+}
+ 
